a09e05989ee328d786b1202757b4678d25565fcb
Guangyu Zhu <guangyuz@gmail.com>
2021-08-11 15:05:25 +0800

    sub

--- vm/src/bytecodes/mod.rs
+++ vm/src/bytecodes/mod.rs
@@ -3,9 +3,11 @@ mod ldu8;
 mod ldu64;
 mod ldu128;
 mod pop;
+mod sub;
 
 pub use add::Add;
 pub use ldu8::LdU8;
 pub use ldu64::LdU64;
 pub use ldu128::LdU128;
 pub use pop::Pop;
+pub use sub::Sub;
--- vm/src/test.rs
+++ vm/src/test.rs
@@ -111,3 +111,25 @@ fn test_add_u128() {
     let expected = Fr::from_hex("0x03").unwrap();
     assert_eq!(result, expected, "result is not expected");
 }
+
+#[test]
+fn test_sub_u8() {
+    let mut cs = DummyCS::new();
+    let mut interp = Interpreter::<Bn256>::new();
+
+    let mut bytecode: Vec<Box<dyn Bytecode<Bn256, DummyCS<Bn256>>>> = Vec::new();
+    bytecode.push(Box::new(LdU8(1u8)));
+    bytecode.push(Box::new(LdU8(2u8)));
+    bytecode.push(Box::new(Sub));
+
+    match interp.run(&mut cs, &bytecode) {
+        Ok(_) => {}
+        Err(e) => {
+            println!("runtime error: {:?}", e)
+        }
+    }
+
+    let result = interp.stack().top().unwrap().value().unwrap();
+    let expected = Fr::from_hex("0x01").unwrap();
+    assert_eq!(result, expected, "result is not expected");
+}
