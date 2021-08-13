4eb19db30003d58d825a2f93a10b8a8b603583b5
Guangyu Zhu <guangyuz@gmail.com>
2021-08-13 17:10:35 +0800

    add vm testsuite entry

--- vm/tests/testsuite.rs
+++ vm/tests/testsuite.rs
@@ -1,6 +1,19 @@
 use std::path::Path;
+use movelang::compiler::compile_script;
+use log::debug;
 
-fn vm_test(_path: &Path) -> datatest_stable::Result<()> {
+fn vm_test(path: &Path) -> datatest_stable::Result<()> {
+    let script_file = path.to_str().expect("path is None.");
+    let compiled_script =  compile_script(script_file)?;
+    let mut bytecode= None;
+    match compiled_script {
+        Some(script) => {
+            let mut script_bytes = vec![];
+            script.serialize(&mut script_bytes)?;
+            bytecode = Some(script_bytes);
+        }
+        None => debug!("Unable to find script in file {:?}", script_file),
+    };
     Ok(())
 }
 
