fec20b9c4c51fddb912618ddedb175b4b03f1965
luxebeng <luxebeng@gmail.com>
2022-12-08 09:53:55 +0800

    handle arithmetic overflow at vm

--- movelang/src/value.rs
+++ movelang/src/value.rs
@@ -789,8 +789,23 @@ impl<F: FieldExt> Add for Value<F> {
     type Output = VmResult<Self>;
 
     fn add(self, b: Value<F>) -> Self::Output {
+        // implement add based on checked_add API to check arithmetic overflow
+        // let value = self.value().and_then(|a| b.value().map(|b| a + b));
         // todo: handle type mismatch
-        let value = self.value().and_then(|a| b.value().map(|b| a + b));
+        let lhs = self.value().unwrap().get_lower_128();
+        let rhs = b.value().unwrap().get_lower_128();
+        let value = match (self.ty(), b.ty()) {
+            (MoveValueType::U8, MoveValueType::U8) => Some(F::from_u128(
+                u8::checked_add(lhs as u8, rhs as u8).expect("arithmetic error found") as u128,
+            )),
+            (MoveValueType::U64, MoveValueType::U64) => Some(F::from_u128(
+                u64::checked_add(lhs as u64, rhs as u64).expect("arithmetic error found") as u128,
+            )),
+            (MoveValueType::U128, MoveValueType::U128) => Some(F::from_u128(
+                u128::checked_add(lhs, rhs).expect("arithmetic error found"),
+            )),
+            (_, _) => unimplemented!(),
+        };
         let c = Value::new_variable(value, None, self.ty())?;
         Ok(c)
     }
@@ -800,7 +815,23 @@ impl<F: FieldExt> Sub for Value<F> {
     type Output = VmResult<Self>;
 
     fn sub(self, b: Value<F>) -> Self::Output {
-        let value = self.value().and_then(|a| b.value().map(|b| a - b));
+        // implement sub based on checked_sub API to check arithmetic overflow
+        //let value = self.value().and_then(|a| b.value().map(|b| a - b));
+        // todo: handle type mismatch
+        let lhs = self.value().unwrap().get_lower_128();
+        let rhs = b.value().unwrap().get_lower_128();
+        let value = match (self.ty(), b.ty()) {
+            (MoveValueType::U8, MoveValueType::U8) => Some(F::from_u128(
+                u8::checked_sub(lhs as u8, rhs as u8).expect("arithmetic error found") as u128,
+            )),
+            (MoveValueType::U64, MoveValueType::U64) => Some(F::from_u128(
+                u64::checked_sub(lhs as u64, rhs as u64).expect("arithmetic error found") as u128,
+            )),
+            (MoveValueType::U128, MoveValueType::U128) => Some(F::from_u128(
+                u128::checked_sub(lhs, rhs).expect("arithmetic error found"),
+            )),
+            (_, _) => unimplemented!(),
+        };
         let c = Value::new_variable(value, None, self.ty())?;
         Ok(c)
     }
@@ -810,7 +841,23 @@ impl<F: FieldExt> Mul for Value<F> {
     type Output = VmResult<Self>;
 
     fn mul(self, b: Value<F>) -> Self::Output {
-        let value = self.value().and_then(|a| b.value().map(|b| a * b));
+        // implement mul based on checked_mul API to check arithmetic overflow
+        // let value = self.value().and_then(|a| b.value().map(|b| a * b));
+        // todo: handle type mismatch
+        let lhs = self.value().unwrap().get_lower_128();
+        let rhs = b.value().unwrap().get_lower_128();
+        let value = match (self.ty(), b.ty()) {
+            (MoveValueType::U8, MoveValueType::U8) => Some(F::from_u128(
+                u8::checked_mul(lhs as u8, rhs as u8).expect("arithmetic error found") as u128,
+            )),
+            (MoveValueType::U64, MoveValueType::U64) => Some(F::from_u128(
+                u64::checked_mul(lhs as u64, rhs as u64).expect("arithmetic error found") as u128,
+            )),
+            (MoveValueType::U128, MoveValueType::U128) => Some(F::from_u128(
+                u128::checked_mul(lhs, rhs).expect("arithmetic error found"),
+            )),
+            (_, _) => unimplemented!(),
+        };
         let c = Value::new_variable(value, None, self.ty())?;
         Ok(c)
     }
