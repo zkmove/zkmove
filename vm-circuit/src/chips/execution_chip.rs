dc28ef181429565400d1782916c0cddfbfa48fab
luxebeng <luxebeng@gmail.com>
2023-01-29 20:57:57 +0800

    code optimize for bitwise op

--- vm-circuit/src/chips/execution_chip.rs
+++ vm-circuit/src/chips/execution_chip.rs
@@ -199,37 +199,22 @@ impl<F: FieldExt> ExecutionChip<F> {
         // bitwise table
         // only 4 bits bitwised every time. so table size is 16*16
         let mut bitwise_values = Vec::new();
-        for value_1 in 0..16 {
-            for value_2 in 0..16 {
-                let field_values = vec![
-                    F::from_u128(Opcode::BitAnd.index() as u128),
-                    F::from_u128(value_1 as u128),
-                    F::from_u128(value_2 as u128),
-                    F::from_u128((value_1 & value_2) as u128),
-                ];
-                bitwise_values.push(field_values);
-            }
-        }
-        for value_1 in 0..16 {
-            for value_2 in 0..16 {
-                let field_values = vec![
-                    F::from_u128(Opcode::BitOr.index() as u128),
-                    F::from_u128(value_1 as u128),
-                    F::from_u128(value_2 as u128),
-                    F::from_u128((value_1 | value_2) as u128),
-                ];
-                bitwise_values.push(field_values);
-            }
-        }
-        for value_1 in 0..16 {
-            for value_2 in 0..16 {
-                let field_values = vec![
-                    F::from_u128(Opcode::Xor.index() as u128),
-                    F::from_u128(value_1 as u128),
-                    F::from_u128(value_2 as u128),
-                    F::from_u128((value_1 ^ value_2) as u128),
-                ];
-                bitwise_values.push(field_values);
+        for op in [Opcode::BitAnd, Opcode::BitOr, Opcode::Xor] {
+            for value_1 in 0..16 {
+                for value_2 in 0..16 {
+                    let field_values = vec![
+                        F::from_u128(op.index() as u128),
+                        F::from_u128(value_1 as u128),
+                        F::from_u128(value_2 as u128),
+                        match op {
+                            Opcode::BitAnd => F::from_u128((value_1 & value_2) as u128),
+                            Opcode::BitOr => F::from_u128((value_1 | value_2) as u128),
+                            Opcode::Xor => F::from_u128((value_1 ^ value_2) as u128),
+                            _ => unreachable!(),
+                        },
+                    ];
+                    bitwise_values.push(field_values);
+                }
             }
         }
         let bitwise_table_columns = self.config.bitwise_table.columns();
