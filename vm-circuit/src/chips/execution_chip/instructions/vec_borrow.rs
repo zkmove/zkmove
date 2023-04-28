db99961cd311d3e5b656526359ffc46e7943925c
luxebeng <luxebeng@gmail.com>
2023-04-28 19:52:26 +0800

    solve conflict

--- vm-circuit/src/chips/execution_chip/instructions/vec_borrow.rs
+++ vm-circuit/src/chips/execution_chip/instructions/vec_borrow.rs
@@ -1,28 +1,40 @@
 // Copyright (c) zkMove Authors
 
-use crate::chips::execution_chip::instructions::Instructions;
+use crate::chips::execution_chip::instructions::InstructionGadget;
 use crate::chips::execution_chip::lookup_tables::LookupsWithCondition;
+use crate::chips::execution_chip::opcode::Opcode;
 use crate::chips::execution_chip::step_chip::StepChipCells;
+use crate::chips::execution_chip::utils::constraint_builder::ConstraintBuilder;
 use crate::witness::execution_steps::ExecutionStep;
 use crate::witness::rw_operations::RWOperations;
 use halo2_proofs::arithmetic::FieldExt;
 use halo2_proofs::circuit::Region;
-use halo2_proofs::plonk::{Error, Expression};
+use halo2_proofs::plonk::Error;
 use std::marker::PhantomData;
 
+#[derive(Clone, Debug)]
 pub struct VecBorrow<const MUTABLE: bool, F: FieldExt> {
     _marker: PhantomData<F>,
 }
 
-impl<const MUTABLE: bool, F: FieldExt> Instructions<F> for VecBorrow<MUTABLE, F> {
+impl<const MUTABLE: bool, F: FieldExt> InstructionGadget<F> for VecBorrow<MUTABLE, F> {
+    const NAME: &'static str = "VECBORROW";
+
+    const OPCODE: Opcode = if MUTABLE {
+        Opcode::VecMutBorrow
+    } else {
+        Opcode::VecImmBorrow
+    };
     fn configure(
+        &self,
         _cells: &StepChipCells<F>,
-        _constraints: &mut Vec<(&str, Expression<F>)>,
+        _cb: &mut ConstraintBuilder<F>,
         _lookups: &mut LookupsWithCondition<F>,
     ) {
     }
 
     fn assign(
+        &self,
         _region: &mut Region<'_, F>,
         _offset: usize,
         _step: &ExecutionStep<F>,
@@ -30,5 +42,11 @@ impl<const MUTABLE: bool, F: FieldExt> Instructions<F> for VecBorrow<MUTABLE, F>
         _cells: &StepChipCells<F>,
     ) -> Result<(), Error> {
         Ok(())
+    }
+
+    fn construct(_cb: &mut ConstraintBuilder<F>) -> Self {
+        Self {
+            _marker: PhantomData,
+        }
     }
 }
--- vm-circuit/src/chips/execution_chip/instructions/vec_push_back.rs
+++ vm-circuit/src/chips/execution_chip/instructions/vec_push_back.rs
@@ -1,28 +1,37 @@
 // Copyright (c) zkMove Authors
 
-use crate::chips::execution_chip::instructions::Instructions;
+use crate::chips::execution_chip::instructions::InstructionGadget;
 use crate::chips::execution_chip::lookup_tables::LookupsWithCondition;
+use crate::chips::execution_chip::opcode::Opcode;
 use crate::chips::execution_chip::step_chip::StepChipCells;
+use crate::chips::execution_chip::utils::constraint_builder::ConstraintBuilder;
 use crate::witness::execution_steps::ExecutionStep;
 use crate::witness::rw_operations::RWOperations;
 use halo2_proofs::arithmetic::FieldExt;
 use halo2_proofs::circuit::Region;
-use halo2_proofs::plonk::{Error, Expression};
+use halo2_proofs::plonk::Error;
 use std::marker::PhantomData;
 
+#[derive(Clone, Debug)]
 pub struct VecPushBack<F: FieldExt> {
     _marker: PhantomData<F>,
 }
 
-impl<F: FieldExt> Instructions<F> for VecPushBack<F> {
+impl<F: FieldExt> InstructionGadget<F> for VecPushBack<F> {
+    const NAME: &'static str = "VECPUSHBACK";
+
+    const OPCODE: Opcode = Opcode::VecPushBack;
+
     fn configure(
+        &self,
         _cells: &StepChipCells<F>,
-        _constraints: &mut Vec<(&str, Expression<F>)>,
+        _cb: &mut ConstraintBuilder<F>,
         _lookups: &mut LookupsWithCondition<F>,
     ) {
     }
 
     fn assign(
+        &self,
         _region: &mut Region<'_, F>,
         _offset: usize,
         _step: &ExecutionStep<F>,
@@ -30,5 +39,11 @@ impl<F: FieldExt> Instructions<F> for VecPushBack<F> {
         _cells: &StepChipCells<F>,
     ) -> Result<(), Error> {
         Ok(())
+    }
+
+    fn construct(_cb: &mut ConstraintBuilder<F>) -> Self {
+        Self {
+            _marker: PhantomData,
+        }
     }
 }
