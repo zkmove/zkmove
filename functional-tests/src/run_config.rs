63dac1513f37b378296681b8c4d39607a5f4283a
caojiafeng <funfriendcjf@gmail.com>
2023-05-25 16:47:12 +0800

    chore: support new-arg/ty_args in test, and also global_ops_num

--- functional-tests/src/run_config.rs
+++ functional-tests/src/run_config.rs
@@ -29,12 +29,15 @@ pub enum Circuit {
 pub struct RunConfig {
     pub signer: Option<Signer>,
     pub args: Option<ScriptArguments>,
+    pub new_args: Option<ScriptArguments>,
     pub ty_args: Vec<TypeTag>,
+    pub new_ty_args: Vec<TypeTag>,
     pub modules: Vec<String>,
     pub circuit: Option<Circuit>,
     pub steps_num: Option<usize>,
     pub stack_ops_num: Option<usize>,
     pub locals_ops_num: Option<usize>,
+    pub global_ops_num: Option<usize>,
 }
 
 impl RunConfig {
@@ -43,11 +46,14 @@ impl RunConfig {
             signer: None,
             args: None,
             ty_args: vec![],
+            new_args: None,
+            new_ty_args: vec![],
             modules: vec![],
             circuit: None,
             steps_num: None,
             stack_ops_num: None,
             locals_ops_num: None,
+            global_ops_num: None,
         };
         let file_str = script_file.to_str().expect("path is None.");
 
@@ -64,8 +70,14 @@ impl RunConfig {
             if let Some(s) = s.strip_prefix("//!args:") {
                 config.args = Some(s.parse::<ScriptArguments>()?);
             }
+            if let Some(s) = s.strip_prefix("//!new_args:") {
+                config.new_args = Some(s.parse()?);
+            }
             if let Some(s) = s.strip_prefix("//!ty_args:") {
                 config.ty_args = parse_type_tags(s)?;
+            }
+            if let Some(s) = s.strip_prefix("//!new_ty_args:") {
+                config.new_ty_args = parse_type_tags(s)?;
             }
             if let Some(s) = s.strip_prefix("//!mods:") {
                 config.modules.push(s.to_string()); //todo: support multiple modules
@@ -81,6 +93,9 @@ impl RunConfig {
             }
             if let Some(s) = s.strip_prefix("//!locals_ops_num:") {
                 config.locals_ops_num = Some(s.parse::<usize>()?);
+            }
+            if let Some(s) = s.strip_prefix("//!global_ops_num:") {
+                config.global_ops_num = Some(s.parse::<usize>()?);
             }
         }
         Ok(config)
--- functional-tests/tests/scripts/call_generic.move
+++ functional-tests/tests/scripts/call_generic.move
@@ -1,6 +1,8 @@
 //! mods: generics.move
 //! ty_args: u8,bool
 //! args: 10u8, false
+//! new_args: false,10u8
+//! new_ty_args: bool,u8
 script {
     use 0x1::Generics;
     fun main<T: copy+drop, S: copy+drop>(t: T, s: S) {
