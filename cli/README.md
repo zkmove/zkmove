90be4c7a64e52ee50eea5673ad5d71aad3ff05d9
Greg Zhu <guangyuz@gmail.com>
2024-12-09 14:59:38 +0800

    add zkmove cli (#334)

---
+++ cli/README.md
@@ -0,0 +1,26 @@
+## CLI for zkMove Virtual Machine
+
+Let us introduce the usage of CLI through an example. Before you begin, make sure you have a customized version of the Move CLI installed.
+
+```shell
+cargo install --git https://github.com/zkmove/aptos-core move-cli --branch witnessing
+```
+
+First, build CLI for zkMove.
+```shell
+cargo build --bin zkmove --release --artifact-dir ./cli/example/ -Z unstable-options
+cd cli/example
+```
+Build and publish the example. Then generate the witness while executing the example. By default, the witness will be generated in a directory called `witnesses`.
+```shell
+move build
+move sandbox publish --skip-fetch-latest-git-deps --ignore-breaking-changes
+move sandbox run --skip-fetch-latest-git-deps --witness storage/0x0000000000000000000000000000000000000000000000000000000000000001/modules/fibonacci.mv test_fibonacci --args 10u64
+```
+
+Finally, execute the “zkmove run” command, which will run the full sequence of setup, proving and verification. Upon successful execution, it will also report the proof size, proving time, and verification time.
+
+```shell
+# Don't forget to replace the witness filename with your own.
+zkmove run -p example -w witnesses/test_fibonacci-1733485309514.json
+```
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,32 +1,22 @@
-use error::VmResult;
-use functional_tests::run_config::RunConfig;
+use anyhow::Result;
+use aptos_move_witnesses::static_info::StaticInfo;
+use aptos_move_witnesses::witness_preprocessor::WitnessPreProcessor;
+use aptos_move_witnesses::{Footprint, Operation};
 use halo2_proofs::halo2curves::bn256::{Bn256, Fr};
-use halo2_proofs::halo2curves::pasta::{EqAffine, Fp};
-use halo2_proofs::poly::commitment::ParamsProver;
-use halo2_proofs::poly::ipa::commitment::ParamsIPA;
 use halo2_proofs::poly::kzg::commitment::ParamsKZG;
 use logger::prelude::*;
-use move_binary_format::CompiledModule;
-use movelang::argument::{parse_transaction_argument, ScriptArgument, ScriptArguments};
-use movelang::compiler::compile_source_files;
-use movelang::generic_call_graph::{generate, RemoteStore};
-use std::marker::PhantomData;
+use move_package::compilation::compiled_package::OnDiskCompiledPackage;
+use move_package::compilation::package_layout::CompiledPackageLayout;
+use move_package::source_package::layout::SourcePackageLayout;
 use std::path::{Path, PathBuf};
 use std::process::exit;
 use structopt::StructOpt;
-use vm::runtime::Runtime;
-use vm::state::StateStore;
-use vm_circuit::circuit::VmCircuit;
-use vm_circuit::witness::CircuitConfig;
-
-use rand::{rngs::StdRng, SeedableRng};
-use vm_circuit::{
-    find_best_k, mock_prove_circuit, print_circuit_layout, prove_vm_circuit_ipa,
-    prove_vm_circuit_kzg, setup_vm_circuit,
-};
+use vm_circuit::circuit_v2::VmCircuit;
+use vm_circuit::witness::{CircuitConfigV2, WitnessV2};
+use vm_circuit::{mock_prove_circuit, prove_and_verify_kzg, setup_circuit, SubCircuit};
 
 #[derive(StructOpt)]
-#[structopt(name = "zkmove", about = "CLI for zkMove Virtual Machine")]
+#[structopt(name = "zkmove", about = "CLI for zkMove")]
 pub struct Arguments {
     #[structopt(subcommand)]
     cmd: Command,
@@ -36,328 +26,69 @@ pub struct Arguments {
 pub enum Command {
     #[structopt(
         name = "run",
-        about = "Run the full sequence of circuit building, setup, proving, and verifying."
+        about = "Run the full sequence of setup, proving, and verification."
     )]
     Run {
         #[structopt(
-            short = "s",
-            long = "script-file",
-            help = "path to .move file containing script"
+            short = "w",
+            long = "witness",
+            help = "path to .json file containing witness"
         )]
-        script: PathBuf,
+        witness: PathBuf,
 
-        #[structopt(
-            short = "m",
-            long = "module-dir",
-            help = "directory containing modules"
-        )]
-        modules: Option<PathBuf>,
+        #[structopt(short = "p", long = "package", help = "move package name")]
+        package: String,
 
         #[structopt(short = "d", long = "debug", help = "debug with mock prover")]
-        use_mock: bool,
-
-        #[structopt(
-            long = "new-args",
-            help = "run with new arguments, still use the old proving/verifying keys, multiple args should separate with space",
-            parse(try_from_str = parse_transaction_argument)
-        )]
-        new_args: Option<Vec<ScriptArgument>>,
-
-        #[structopt(short = "v", long = "verbose")]
-        verbose: bool,
-
-        #[structopt(long = "print-layout")]
-        print_layout: bool,
-
-        #[structopt(long = "pcs", help = "polynomial commitment scheme")]
-        pcs: Option<String>,
+        debug: bool,
     },
-    #[structopt(name = "graph", about = "generate generic call graph")]
-    CallGraph { module: PathBuf, output: PathBuf },
+    // #[structopt(name = "graph", about = "generate generic call graph")]
+    // CallGraph { module: PathBuf, output: PathBuf },
 }
 
 impl Arguments {
-    #[allow(clippy::too_many_arguments)]
-    pub fn run(
-        &self,
-        script: &Path,
-        module_dir: &Option<PathBuf>,
-        use_mock: bool,
-        new_args: &Option<Vec<ScriptArgument>>,
-        verbose: bool,
-        print_layout: bool,
-        pcs: &Option<String>,
-    ) -> VmResult<()> {
-        logger::init_for_main(verbose);
+    pub fn run(&self, witness: &Path, package: &String, debug: bool) -> Result<()> {
+        logger::init_for_main(debug);
 
-        let ty = {
-            if let Some(s) = pcs {
-                s.to_lowercase()
-            } else {
-                String::from("kzg")
-            }
+        debug!("witness {:?}", witness.display());
+
+        // Always root ourselves to the package root, and then compile relative to that.
+        let rooted_path = SourcePackageLayout::try_find_root(&witness.canonicalize()?)?;
+        let build_path = rooted_path
+            .join(CompiledPackageLayout::Root.path())
+            .join(package.as_str());
+        let package = OnDiskCompiledPackage::from_path(build_path.as_path())?;
+        let package = package.into_compiled_package()?;
+        let trace_contents = std::fs::read_to_string(witness)?;
+        let traces: Vec<Footprint> = serde_json::from_str(&trace_contents)?;
+        let entry = match &traces.first().unwrap().data {
+            Operation::Start { entry_call } => entry_call,
+            _ => unreachable!(),
         };
-        if ty.eq(&String::from("ipa")) {
-            info!("PCS is implemented with IPA");
-            self.run_ipa(script, module_dir, use_mock, new_args, print_layout)
-        } else if ty.eq(&String::from("kzg")) {
-            info!("PCS is implemented with KZG");
-            self.run_kzg(script, module_dir, use_mock, new_args, print_layout)
-        } else {
-            error!("only IPA and KZG supported");
-            Ok(())
-        }
-    }
+        let static_info = StaticInfo::generate(
+            entry.module_id.as_ref().unwrap(),
+            entry.function_index as u16,
+            &package,
+        );
+        let preprocessor = WitnessPreProcessor::default();
+        let states = preprocessor.pre_process(&traces, &static_info);
+        let witness = WitnessV2::new(states, static_info, CircuitConfigV2::default());
+        let circuit = VmCircuit::<Fr>::new_from_witness(&witness);
 
-    #[allow(clippy::too_many_arguments)]
-    pub fn run_kzg(
-        &self,
-        script: &Path,
-        module_dir: &Option<PathBuf>,
-        use_mock: bool,
-        new_args: &Option<Vec<ScriptArgument>>,
-        print_layout: bool,
-    ) -> VmResult<()> {
-        let script_file = script.to_str().expect("path is None.");
+        let k = 12; //TODO: auto pick best k
 
-        // compile script and depended modules
-        let mut targets = vec![script_file.to_string()];
-        let config = RunConfig::new(script)?;
-
-        for module in config.modules.into_iter() {
-            let path = module_dir
-                .clone()
-                .expect("module_dir is missing")
-                .as_path()
-                .join(module)
-                .to_str()
-                .unwrap()
-                .to_string();
-            targets.push(path);
-        }
-        info!("compile script...{:?}", targets);
-        let (compiled_script, compiled_modules) = compile_source_files(targets)?;
-
-        let script = compiled_script.expect("script is missing");
-        #[cfg(not(target_arch = "wasm32"))]
-        let runtime = Runtime::new()
-            .ext_web3("https://cloudflare-eth.com")
-            .unwrap();
-        #[cfg(target_arch = "wasm32")]
-        let runtime = Runtime::new();
-
-        let mut state = StateStore::new();
-        for module in compiled_modules.clone().into_iter() {
-            state.add_module(module);
+        if debug {
+            debug!("Mock prove");
+            mock_prove_circuit(&circuit, vec![], k)?;
         }
 
-        info!("generate execution trace...");
-        let circuit_config = CircuitConfig::default()
-            .max_step_row(config.step_max_row)
-            .stack_ops_num(config.stack_ops_num)
-            .locals_ops_num(config.locals_ops_num)
-            .global_ops_num(config.global_ops_num)
-            .word_size(config.word_capacity);
-        let trace = runtime.execute_script(
-            script.clone(),
-            config.ty_args.clone(),
-            config.signer.clone(),
-            config.args,
-            &mut state,
-        )?;
-        let witness = runtime.process_execution_trace(
-            config.ty_args.clone(),
-            Some(script.clone()),
-            None,
-            compiled_modules.clone(),
-            trace,
-            circuit_config.clone(),
-        )?;
-
-        let vm_circuit = VmCircuit {
-            witness,
-            public_input: None,
-            _maker: PhantomData,
-        };
-        info!("find the best k...");
-        let k = find_best_k(&vm_circuit);
-        info!("k = {}", k);
-
-        if use_mock {
-            info!("run with mock prover...");
-            mock_prove_circuit(&vm_circuit, vec![vec![Fr::zero()]], k)?;
-        }
-
-        if print_layout {
-            info!("print circuit layout into layout.svg ...");
-            print_circuit_layout(k, &vm_circuit);
-        }
-
-        info!("setup vm circuit...");
-        let rng = StdRng::from_entropy();
+        debug!("Generate parameters");
+        let rng = rand::rngs::mock::StepRng::new(0, 1);
         let params = ParamsKZG::<Bn256>::setup(k, rng);
-        let (_, pk) = setup_vm_circuit(&vm_circuit, &params)?;
+        let (_, pk) = setup_circuit(&circuit, &params)?;
 
-        info!("prove vm circuit...");
-        prove_vm_circuit_kzg(vm_circuit, &[&[Fr::zero()]], &params, pk.clone())?;
-        #[allow(clippy::or_fun_call)]
-        if let Some(new_args) = new_args
-            .as_ref()
-            .or(config.new_args.as_ref().map(|t| t.as_inner()))
-        {
-            info!("execute script with new arguments");
-            let arguments = Some(ScriptArguments::new(new_args.clone()));
-            let new_ty_args = if config.new_ty_args.is_empty() {
-                config.ty_args
-            } else {
-                config.new_ty_args
-            };
-            let new_trace = runtime.execute_script(
-                script.clone(),
-                new_ty_args.clone(),
-                config.signer,
-                arguments,
-                &mut state,
-            )?;
-            let new_witness = runtime.process_execution_trace(
-                new_ty_args,
-                Some(script),
-                None,
-                compiled_modules,
-                new_trace,
-                circuit_config,
-            )?;
-            let new_vm_circuit = VmCircuit {
-                witness: new_witness,
-                public_input: None,
-                _maker: PhantomData,
-            };
-            info!("prove the new execution with old proving key...");
-            prove_vm_circuit_kzg(new_vm_circuit, &[&[Fr::zero()]], &params, pk)?;
-        }
-
-        Ok(())
-    }
-
-    #[allow(clippy::too_many_arguments)]
-    pub fn run_ipa(
-        &self,
-        script: &Path,
-        module_dir: &Option<PathBuf>,
-        use_mock: bool,
-        new_args: &Option<Vec<ScriptArgument>>,
-        print_layout: bool,
-    ) -> VmResult<()> {
-        let script_file = script.to_str().expect("path is None.");
-
-        // compile script and depended modules
-        let mut targets = vec![script_file.to_string()];
-        let config = RunConfig::new(script)?;
-
-        for module in config.modules.into_iter() {
-            let path = module_dir
-                .clone()
-                .expect("module_dir is missing")
-                .as_path()
-                .join(module)
-                .to_str()
-                .unwrap()
-                .to_string();
-            targets.push(path);
-        }
-        info!("compile script...");
-        let (compiled_script, compiled_modules) = compile_source_files(targets)?;
-
-        let script = compiled_script.expect("script is missing");
-        let runtime = Runtime::new();
-        let mut state = StateStore::new();
-        for module in compiled_modules.clone().into_iter() {
-            state.add_module(module);
-        }
-
-        info!("generate execution trace...");
-        let circuit_config = CircuitConfig::default()
-            .max_step_row(config.step_max_row)
-            .stack_ops_num(config.stack_ops_num)
-            .locals_ops_num(config.locals_ops_num)
-            .global_ops_num(config.global_ops_num)
-            .word_size(config.word_capacity);
-        let trace = runtime.execute_script(
-            script.clone(),
-            config.ty_args.clone(),
-            config.signer.clone(),
-            config.args,
-            &mut state,
-        )?;
-        let witness = runtime.process_execution_trace(
-            config.ty_args.clone(),
-            Some(script.clone()),
-            None,
-            compiled_modules.clone(),
-            trace,
-            circuit_config.clone(),
-        )?;
-        let vm_circuit = VmCircuit {
-            witness,
-            public_input: None,
-            _maker: PhantomData,
-        };
-        info!("find the best k...");
-        let k = find_best_k(&vm_circuit);
-        info!("k = {}", k);
-
-        if use_mock {
-            info!("run with mock prover...");
-            mock_prove_circuit(&vm_circuit, vec![vec![Fp::zero()]], k)?;
-        }
-
-        if print_layout {
-            info!("print circuit layout into layout.svg ...");
-            print_circuit_layout(k, &vm_circuit);
-        }
-
-        info!("setup vm circuit...");
-        let params: ParamsIPA<EqAffine> = ParamsIPA::new(k);
-        let (_, pk) = setup_vm_circuit(&vm_circuit, &params)?;
-
-        info!("prove vm circuit...");
-        prove_vm_circuit_ipa(vm_circuit, &[&[Fp::zero()]], &params, pk.clone())?;
-        #[allow(clippy::or_fun_call)]
-        if let Some(new_args) = new_args
-            .as_ref()
-            .or(config.new_args.as_ref().map(|t| t.as_inner()))
-        {
-            info!("execute script with new arguments");
-            let arguments = Some(ScriptArguments::new(new_args.clone()));
-            let new_ty_args = if config.new_ty_args.is_empty() {
-                config.ty_args
-            } else {
-                config.new_ty_args
-            };
-            let new_trace = runtime.execute_script(
-                script.clone(),
-                new_ty_args.clone(),
-                config.signer,
-                arguments,
-                &mut state,
-            )?;
-
-            let new_witness = runtime.process_execution_trace(
-                new_ty_args,
-                Some(script),
-                None,
-                compiled_modules,
-                new_trace,
-                circuit_config,
-            )?;
-            let new_vm_circuit = VmCircuit {
-                witness: new_witness,
-                public_input: None,
-                _maker: PhantomData,
-            };
-            info!("prove the new execution with old proving key...");
-            prove_vm_circuit_ipa(new_vm_circuit, &[&[Fp::zero()]], &params, pk)?;
-        }
+        debug!("Generate zk proof");
+        prove_and_verify_kzg(circuit, &[], &params, pk.clone());
 
         Ok(())
     }
@@ -368,38 +99,26 @@ fn main() {
 
     let result = match args.cmd {
         Command::Run {
-            ref script,
-            ref modules,
-            use_mock,
-            ref new_args,
-            verbose,
-            print_layout,
-            ref pcs,
-        } => args.run(
-            script.as_path(),
-            modules,
-            use_mock,
-            new_args,
-            verbose,
-            print_layout,
-            pcs,
-        ),
-        Command::CallGraph { module, output } => {
-            std::fs::create_dir_all(output.as_path()).unwrap();
-            let module =
-                CompiledModule::deserialize(std::fs::read(module.as_path()).unwrap().as_ref())
-                    .unwrap();
-            let store = {
-                let mut s = RemoteStore::default();
-                s.add_module(&module);
-                s
-            };
-            let graphs = generate(&module.self_id(), &store);
-            for (fname, graph) in graphs {
-                std::fs::write(output.join(fname).with_extension("dot"), graph.to_dot()).unwrap();
-            }
-            Ok(())
-        }
+            ref witness,
+            ref package,
+            debug,
+        } => args.run(witness.as_path(), package, debug),
+        // Command::CallGraph { module, output } => {
+        //     std::fs::create_dir_all(output.as_path()).unwrap();
+        //     let module =
+        //         CompiledModule::deserialize(std::fs::read(module.as_path()).unwrap().as_ref())
+        //             .unwrap();
+        //     let store = {
+        //         let mut s = RemoteStore::default();
+        //         s.add_module(&module);
+        //         s
+        //     };
+        //     let graphs = generate(&module.self_id(), &store);
+        //     for (fname, graph) in graphs {
+        //         std::fs::write(output.join(fname).with_extension("dot"), graph.to_dot()).unwrap();
+        //     }
+        //     Ok(())
+        // }
     };
 
     if let Err(error) = result {
